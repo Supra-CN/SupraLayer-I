c913ce858c122a3a0bde846f7d59debb9f07cb3d
Supra.CN <Supra.CN@gmail.com>
2013-10-14 17:55:22 +0800

    同步工作库；

--- src/cn/supra/supralayer_i/ui/clients/LauncherClient.java
+++ src/cn/supra/supralayer_i/ui/clients/LauncherClient.java
@@ -1,11 +1,14 @@
 package cn.supra.supralayer_i.ui.clients;
 
+import cn.supra.supralayer_i.R;
 import android.content.Context;
+import android.view.View;
 
 public class LauncherClient extends BaseClient{
 
 	public LauncherClient(Context context) {
 		super(context);
+		View.inflate(context, R.layout.launcher, this);
 	}
 
 }
